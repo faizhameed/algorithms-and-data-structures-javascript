95a4e2c485e9da676752027173342db452d4faaf
faizhameed <faizftb@gmail.com>
2020-03-17 22:02:44 +0530

    problem 27 Euler done

--- Project-Euler/Problem 27/problem-27.py
+++ Project-Euler/Problem 27/problem-27.py
@@ -11,6 +11,22 @@ def isPrime(num):
                 return True 
         else: 
             return False
+isPrime(7)
 
-val = isPrime(7)
-print(val)
+a=0
+b=0
+max_n=0
+for i in range(-1000,1000):
+    for j in range(-1000,1000):
+        n=1
+        flag=True
+        while(flag):
+            if(isPrime(n**2+i*n+j)):
+                n+=1
+            else:
+                if(max_n<n):
+                    max_n=n
+                    a=i
+                    b=j
+                flag=False
+print(a*b)
