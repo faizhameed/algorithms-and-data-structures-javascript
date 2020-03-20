86b29a7fc9c6ba371a9920ce6f0dea6f5dbafcec
faizhameed <faizftb@gmail.com>
2020-03-20 21:17:34 +0530

    hacker rank problem added

---
+++ Hacker-Rank/RegularExpressions.js
@@ -0,0 +1,15 @@
+// Day 7: Regular Expressions I
+
+function regexVar() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+   */
+
+  const re = /^([aeiou]).+\1$/;
+
+  /*
+   * Do not remove the return statement
+   */
+  return re;
+}
