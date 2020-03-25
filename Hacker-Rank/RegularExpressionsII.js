165d8f7e93b169d1ccfcd19d4afc7d827d60aca9
faizhameed <faizftb@gmail.com>
2020-03-25 07:37:20 +0530

    Regukar expression 2

---
+++ Hacker-Rank/RegularExpressionsII.js
@@ -0,0 +1,18 @@
+function regexVar() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.',
+   * followed by one or more letters.
+   */
+
+  const re = /^(Mr|Mrs|Ms|Dr|Er)+[.]+([a-zA-Z])+$/;
+  /*
+   * Do not remove the return statement
+   */
+  return re;
+}
+let res = "Mr.Dr".match(regexVar());
+console.log(res);
+let a;
+res ? (a = true) : (a = false);
+console.log(a);
