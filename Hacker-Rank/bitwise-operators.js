b2b72f14e951395bfd23b06347d6f2aa2bb55877
faizhameed <41015883+faizhameed@users.noreply.github.com>
2020-05-18 22:11:54 +0530

    Bitwise Operator problem

---
+++ Hacker-Rank/bitwise-operators.js
@@ -0,0 +1,11 @@
+// We define  to be a sequence of distinct sequential integers from  to ; in other words, . We want to know the maximum bitwise AND value of any two integers,  and  (where ), in sequence  that is also less than a given integer, .
+
+
+function bitwise(n,k){
+    let i =n
+    while(true){
+        i = n%2         
+    }
+}
+
+//to  be completed
