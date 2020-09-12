7795c03365917a5b0fcc355f33ab37cb00e7f255
Faiz Hameed <faizftb@gmail.com>
2020-09-12 21:07:31 +0530

    2nd method rotate matrix

--- cracking_the_coding_interview/Arrays and Strings/zeroMAtrix.js
+++ cracking_the_coding_interview/Arrays and Strings/zeroMAtrix.js
@@ -38,3 +38,43 @@ function zeroMatrix_m1(matrix) {
 }
 
 /* method two (better)*/
+
+function zeroMatrix_m2(matrix, column) {
+  let firstRow = false;
+  let firstCol = false;
+
+  // check if first column or first row has a zero
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] == 0) {
+      firstCol = true;
+    }
+  }
+  for (let j = 0; j < matrix.length; j++) {
+    if (matrix[0][j] == 0) {
+      firstRow = true;
+    }
+  }
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[0].length; j++) {
+      if (matrix[i][j] == 0) {
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
+      }
+    }
+  }
+
+  //nullify columns based on values in first row and first col
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] == 0) {
+      nullifyRow(matrix, i);
+    }
+  }
+
+  for (let j = 0; j < matrix.length; j++) {
+    if (matrix[0][j] == 0) {
+      nullifyCol(matrix, j);
+    }
+  }
+}
