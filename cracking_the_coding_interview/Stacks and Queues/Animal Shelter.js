458e95f76884bea32c2075bab2a02f919912d526
Faiz Hameed <faizftb@gmail.com>
2020-12-27 22:47:32 +0530

    CTCI animals 3.6

--- cracking_the_coding_interview/Stacks and Queues/Animal Shelter.js
+++ cracking_the_coding_interview/Stacks and Queues/Animal Shelter.js
@@ -1,17 +1,33 @@
-class Animal{
-    dogs=linkedList('dog')
-    cats=linkedList('cat')
-    constructor(){
-        this.order =0
+class Animal {
+  dogs = linkedList("dog");
+  cats = linkedList("cat");
+  constructor() {
+    this.order = 0;
+  }
+  enqueue_animal(animal) {
+    if (animal.type === "cat") {
+      if (this.cats.name === null) {
+        this.cats = animal;
+      } else {
+        this.cats.next = animal;
+      }
+      this.cats.order++;
+    } else {
+      if (this.dogs.name === null) {
+        this.dogs = animal;
+      } else {
+        this.dogs.next = animal;
+      }
+      this.dogs.order++;
     }
-    enqueue_animal ()
+  }
 }
 
-function linkedList(type){
-return {
-    name:null,
-    next:null,
-    order:null,
-    type
-}
+function linkedList(type) {
+  return {
+    name: null,
+    next: null,
+    order: null,
+    type,
+  };
 }
