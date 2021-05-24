e5cceae0b15653d37460f5f86aed0647e932e7f1
Faiz Hameed <faizftb@gmail.com>
2021-05-24 08:56:02 +0530

    quickselect algorithm

--- Mastering_Algo_course/Recursion/getKthLargest.js
+++ Mastering_Algo_course/Recursion/getKthLargest.js
@@ -1,5 +1,23 @@
 const getKthLargest = (arr, k) => {
   const indexToFind = arr.length - k;
-  quickSort(arr, 0, arr.length - 1);
+  // quickSort(arr, 0, arr.length - 1);
+
   return arr[indexToFind];
+};
+
+/* with quick Hoare's select algorithm */
+
+const quickSelect = (arr, left, right, indexToFind) => {
+  if (left < right) {
+    const partitionIndex = partition(arr, left, right);
+    if (partitionIndex === indexToFind) {
+      return arr[indexToFind];
+    }
+    if (indexToFind < partitionIndex) {
+      return quickSelect(arr, left, partitionIndex - 1, indexToFind);
+    }
+    if (indexToFind < partitionIndex) {
+      return quickSelect(arr, partitionIndex + 1, right, indexToFind);
+    }
+  }
 };
