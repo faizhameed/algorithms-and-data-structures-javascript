9662966b66ed0e8b7fc0f05ecbe5b927c10a5cfd
Faiz Hameed <faizftb@gmail.com>
2021-06-18 23:11:00 +0530

    search in a 2D sorted matrix

---
+++ Mastering_Algo_course/Matrix/SearchMatrix.js
@@ -0,0 +1,4 @@
+/* If the matrix is sorted we first point to the middle of row and column. 
+Then set new row and new column to search we change the pointer for row and column each time */
+
+var searchMatrix = function (matrix, target) {};
