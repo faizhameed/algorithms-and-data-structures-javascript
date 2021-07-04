06217ca25798b70b6c6ba8050f08d88070bcbdb1
Faiz Hameed <faizftb@gmail.com>
2021-07-04 08:50:55 +0530

    fixing and commenting n graph problem

--- Mastering_Algo_course/Graphs/numberOfMins.js
+++ Mastering_Algo_course/Graphs/numberOfMins.js
@@ -1,11 +1,15 @@
-/* https://leetcode.com/problems/time-needed-to-inform-all-employees/ */
+/* https://leetcode.com/problems/time-needed-to-inform-all-employees/ 
+n number of employees
+
+*/
 const numOFMinutes = (n, head1D, managers, informTime) => {
   const adjList = managers.map(() => []);
   for (let i = 0; i < n; i++) {
-    const manager = managers[e];
+    const manager = managers[i];
     if (manager === -1) continue;
-    adjList[manager].push(e);
+    adjList[manager].push(i);
   }
+  // now we get list of employees under each manager if there is no employee under any employee we have empty array at that index
   return dfs(head1D, adjList, informTime);
 };
 
@@ -18,5 +22,5 @@ const dfs = (currentID, adjList, informTime) => {
   for (let i = 0; i < subOrdinates.length; i++) {
     max = Math.max(max, dfs(subOrdinates[i], adjList, informTime));
   }
-  return max + informTime[currentID];
+  return max + informTime[currentID]; //maxtime to send news to the lower employee + time taken to inform by the current Manager
 };
