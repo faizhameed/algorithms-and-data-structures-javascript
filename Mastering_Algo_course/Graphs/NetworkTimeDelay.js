90da2f7866b3a3634164c4c60085a30cd7b23679
Faiz Hameed <faizftb@gmail.com>
2021-07-16 20:45:08 +0530

    notes on network time

---
+++ Mastering_Algo_course/Graphs/NetworkTimeDelay.js
@@ -0,0 +1,10 @@
+/* 
+Greedy algorithm
+S->A costs 2
+S->B costs 4
+then we take S->A which is less
+at each step we take the least weighted path
+
+*/
+
+const networkTimeDelay = (times, n, k) => {};
