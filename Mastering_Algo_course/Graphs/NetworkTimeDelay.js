a06381bad47fa8d1d26a6b12875d90be5c619a7f
Faiz Hameed <faizftb@gmail.com>
2021-07-19 15:53:17 +0530

    network time delay with priority queue

--- Mastering_Algo_course/Graphs/NetworkTimeDelay.js
+++ Mastering_Algo_course/Graphs/NetworkTimeDelay.js
@@ -4,7 +4,38 @@ S->A costs 2
 S->B costs 4
 then we take S->A which is less
 at each step we take the least weighted path
-
+times[i] = (ui, vi, wi)
 */
 
-const networkTimeDelay = (times, n, k) => {};
+const networkTimeDelay = (times, n, k) => {
+  // create and array with infinite time
+  const distances = new Array(n).fill(Infinity);
+
+  const adjList = distances.map(() => []);
+
+  distances[k - 1] = 0; // since distance from point to same point
+
+  const heap = new PriorityQueue((a, b) => distances[a] < distances[b]);
+  heap.push(k - 1);
+  for (let i = 0; i < times.length; i++) {
+    const sourceNode = times[i][0];
+    const targetNode = times[i][1];
+    const weight = times[i][2];
+    adjList[sourceNode - 1].push([targetNode - 1, weight]);
+  }
+  while (!heap.isEmpty()) {
+    const currentVertex = heap.pop();
+    const adjacent = adjList[currentVertex];
+    const currentWeight = distances[currentVertex];
+    for (let i = 0; i < adjacent.length; i++) {
+      const target = adjacent[i][0];
+      const weight = adjacent[i][1];
+      if (distances[target] > currentWeight + weight) {
+        distances[target] = currentWeight + weight;
+        heap.push(target);
+      }
+    }
+  }
+  const ans = Math.max(...distances);
+  return ans === Infinity ? -1 : ans;
+};
