42dc4054f48a552394fcd88f7bf63c0aa0a954f7
Faiz Hameed <faizftb@gmail.com>
2021-08-06 21:29:29 +0530

    knight probability in a chessboard

--- Mastering_Algo_course/Dynamic Programming/knightProbability.js
+++ Mastering_Algo_course/Dynamic Programming/knightProbability.js
@@ -8,12 +8,20 @@ const DIRECTIONS = [
   [-1, -2],
 ];
 
-const knightProbability = (n, k, row, column) => {
-  const knightP = (k, r, c) => {
-    // base case
-    if (r < 0 || r >= n || c < 0 || c >= n) {
-      return 0;
-    }
-  };
-  knightP(k, row, column);
+const knightP = (n, k, r, c) => {
+  // base case
+  if (r < 0 || r >= n || c < 0 || c >= n) {
+    return 0;
+  }
+  if (k == 0) {
+    return 1;
+  }
+  let res = 0;
+  for (let i = 0; i < DIRECTIONS.length; i++) {
+    const dir = DIRECTIONS[i];
+    res += knightP(n, k - 1, r + dir[0], c + dir[1]) / 8;
+  }
+  return res;
 };
+
+console.log(knightP(4, 3, 2, 2));
