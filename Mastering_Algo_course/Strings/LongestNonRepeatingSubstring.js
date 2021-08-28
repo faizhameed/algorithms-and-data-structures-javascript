e7114b792c36a4c83a1568912bfece8ba20c7707
Faiz Hameed <faizftb@gmail.com>
2021-08-28 11:21:08 +0530

    optimized solution

--- Mastering_Algo_course/Strings/LongestNonRepeatingSubstring.js
+++ Mastering_Algo_course/Strings/LongestNonRepeatingSubstring.js
@@ -4,7 +4,6 @@ var lengthOfLongestSubstring = function (s) {
   }
   let uniqueArr = [],
     maxLength = 1;
-
   for (let i = 0; i < s.length; i++) {
     if (uniqueArr.includes(s[i])) {
       uniqueArr = uniqueArr.slice(uniqueArr.indexOf(s[i]) + 1);
@@ -15,4 +14,21 @@ var lengthOfLongestSubstring = function (s) {
   return maxLength;
 };
 
-console.log(lengthOfLongestSubstring("ffbcdce"));
+var lengthOfLongestSubstringOptimized = function (s) {
+  let left = 0,
+    right = 0,
+    longest = 0;
+  const seen = {};
+  while (right < s.length) {
+    if (seen[s[right]] >= 0 && seen[s[right]] >= left) {
+      // and is this before left?
+      left = seen[s[right]] + 1; // now we can ignore everything that was before
+    }
+    seen[s[right]] = right;
+    longest = Math.max(right - left + 1, longest);
+    right++;
+  }
+  return longest;
+};
+
+console.log(lengthOfLongestSubstringOptimized("ab"));
