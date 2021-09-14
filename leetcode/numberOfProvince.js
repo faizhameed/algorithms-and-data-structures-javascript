79662bed03b59a115f0c14af9f89f3740b00fd6b
Faiz Hameed <faizftb@gmail.com>
2021-09-14 20:46:27 +0530

    number of province var fix

--- leetcode/numberOfProvince.js
+++ leetcode/numberOfProvince.js
@@ -1,10 +1,9 @@
-https://leetcode.com/problems/number-of-provinces/
-
-
+//leetcode.com/problems/number-of-provinces/
+//code_l:js
 const findCircleNum = (isConnected) => {
   const size = isConnected.length;
   const adjList = new Array(size).fill(0).map(() => []);
-  let count = 0;
+  let provinceCount = 0;
   let isVisited = new Array(size).fill(false);
   // make a adjacent list visits from each node.
   for (let i = 0; i < size; i++) {
@@ -14,19 +13,22 @@ const findCircleNum = (isConnected) => {
       }
     }
   }
-  const queue = [];// initialising a queue that takes all the cities in a province
+  const queue = []; // initialising a queue that takes all the cities in a province
   for (let a = 0; a < adjList.length; a++) {
     if (!isVisited[a]) {
-        // if not visited then this is a new provinces so count ++
-      count++;
+      // if not visited then this is a new provinces so count ++
+      provinceCount++;
     } else {
       continue;
     }
-    queue.push(adjList[a]);// push the adjacent list at the current province
-    while (queue.length > 0) {// we will visit all the places that is directly and indirectly connected with a by utilising the queue
+    queue.push(adjList[a]); // push the adjacent list at the current province
+    while (queue.length > 0) {
+      // we will visit all the places that is directly and indirectly connected with a by utilising the queue
       const curr = queue.shift();
-      for (let i = 0; i < curr.length; i++) {// here we visit all the place directly connected with city a
-        if (isVisited[curr[i]]) {// we have alread visited here either its in the queue or its direct connections are already iterated
+      for (let i = 0; i < curr.length; i++) {
+        // here we visit all the place directly connected with city a
+        if (isVisited[curr[i]]) {
+          // we have alread visited here either its in the queue or its direct connections are already iterated
           continue;
         } else {
           queue.push(adjList[curr[i]]);
@@ -35,7 +37,13 @@ const findCircleNum = (isConnected) => {
       }
     }
   }
-  return count;
+  return provinceCount;
 };
 
-console.log(findCircleNum([[[0]]]));
+console.log(
+  findCircleNum([
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
+  ])
+);
