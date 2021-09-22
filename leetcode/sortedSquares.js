f57f727e91871afcd973cc03b181d2f2cf99967d
Faiz Hameed <faizftb@gmail.com>
2021-09-22 22:43:27 +0530

    Revert "Dev"

--- leetcode/sortedSquares.js
+++
@@ -1,9 +0,0 @@
-var sortedSquares = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
-    nums[i] = nums[i] * nums[i];
-  }
-  nums.sort((a, b) => a - b);
-  return nums;
-};
-
-console.log(sortedSquares([-4, -1, 0, 3, 10]));
