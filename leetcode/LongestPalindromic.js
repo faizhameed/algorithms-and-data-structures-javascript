6a6018ae1ce9ea2e3025779218cd4f435d4b7eeb
Faiz Hameed <faizftb@gmail.com>
2021-09-24 14:56:19 +0530

    longest palindrome substring

--- leetcode/LongestPalindromic.js
+++ leetcode/LongestPalindromic.js
@@ -16,23 +16,66 @@ var longestPalindrome = function (s) {
     }
     return substring(s, start + 1, end);
   }
-  function isPalindrome(s) {
-    let first = 0,
-      end = s.length - 1;
-    while (first < end) {
-      if (s[first] === s[end]) {
-        first++;
-        end--;
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
 
   substring(s, 0, 0);
 
   return res.reduce((a, v) => (a.length >= v.length ? a : v), res[0]);
 };
+function isPalindrome(s) {
+  let first = 0,
+    end = s.length - 1;
+  while (first < end) {
+    if (s[first] === s[end]) {
+      first++;
+      end--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
 
-console.log(longestPalindrome("babad"));
+const longestPalindromeOpt = (s) => {
+  const pal = [];
+  const n = s.length;
+  for (let start = 0; start < n; start++) {
+    for (let end = n - 1; end >= start; end--) {
+      if (s[start] === s[end] && isPalindrome(s.slice(start, end + 1))) {
+        pal.push(s.slice(start, end + 1));
+        break;
+      }
+    }
+  }
+
+  return pal.reduce((a, v) => (a.length >= v.length ? a : v), pal[0]);
+};
+
+/* further optimized solution */
+
+const longestPalindromeOptEven = (s) => {
+  if (s.length <= 1) return s;
+  let start = 0,
+    end = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let len1 = expandFromCenter(s, i, i);
+    let len2 = expandFromCenter(s, i, i + 1);
+    const len = Math.max(len1, len2);
+    if (len > end - start) {
+      start = Math.ceil(i - (len - 1) / 2);
+      end = Math.floor(i + len / 2);
+    }
+  }
+  return s.slice(start, end + 1);
+
+  function expandFromCenter(s, l, r) {
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      l--;
+      r++;
+    }
+
+    return r - l - 1;
+  }
+};
+
+console.log(longestPalindromeOptEven("babaaabbaa"));
