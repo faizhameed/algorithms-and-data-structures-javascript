25611fce7b025383cd38038f2df2438b9c16ef7e
Faiz Hameed <faizftb@gmail.com>
2021-09-24 19:35:16 +0530

    tribonoci more optimized solution

--- leetcode/TribonocciSeries.js
+++ leetcode/TribonocciSeries.js
@@ -8,3 +8,16 @@ var tribonacci = function (n) {
   map[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
   return map[n];
 };
+
+/* No recursive optimized */
+
+var tribonacci = function (n) {
+  let arr = [0, 1, 1];
+  if (n < 3) {
+    return arr[n];
+  }
+  for (let i = 3; i <= n; i++) {
+    arr.push(arr[i - 3] + arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+};
