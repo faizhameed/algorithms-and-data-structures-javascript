e012b7d09aa06679872ca58b655ff42154b192e0
Faiz Hameed <faizftb@gmail.com>
2021-10-04 15:04:43 +0530

    optimized DP solution for matrixUpdate

--- leetcode/updateMatrix.js
+++ leetcode/updateMatrix.js
@@ -22,7 +22,6 @@ var updateMatrix = function (mat) {
 const directions = [
   [1, 0],
   [-1, 0],
-
   [0, 1],
   [0, -1],
 ];
@@ -82,4 +81,36 @@ const mat = [
   [1, 1, 1, 1, 0, 1, 0, 0, 1, 1],
 ];
 
-console.log(updateMatrix(mat));
+console.log(updateMatrixOptimized(mat));
+
+// instead of working from 1 lets work out from all the zeros,all the adjacent of zeros get 1,
+
+function updateMatrixOptimized(mat) {
+  const rows = mat.length;
+  if (rows === 0) return mat;
+  const cols = mat[0].length;
+  const dist = new Array(mat.length)
+    .fill(0)
+    .map(() => new Array(mat[0].length).fill(Infinity));
+
+  //First pass: check for left and top
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (mat[i][j] === 0) {
+        dist[i][j] = 0;
+      } else {
+        if (i > 0) dist[i][j] = Math.min(dist[i][j], dist[i - 1][j] + 1);
+        if (j > 0) dist[i][j] = Math.min(dist[i][j], dist[i][j - 1] + 1);
+      }
+    }
+  }
+
+  //Second pass: check for bottom and right
+  for (let i = rows - 1; i >= 0; i--) {
+    for (let j = cols - 1; j >= 0; j--) {
+      if (i < rows - 1) dist[i][j] = Math.min(dist[i][j], dist[i + 1][j] + 1);
+      if (j < cols - 1) dist[i][j] = Math.min(dist[i][j], dist[i][j + 1] + 1);
+    }
+  }
+  return dist;
+}
