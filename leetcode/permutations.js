5f1387c7a0a95e6e64ae679badd980a34dcc9a86
Faiz Hameed <faizftb@gmail.com>
2021-10-05 13:59:08 +0530

    permute unique

--- leetcode/permutations.js
+++ leetcode/permutations.js
@@ -9,3 +9,17 @@ var permute = function (nums, curr = [], res = []) {
   }
   return res;
 };
+
+var permuteUnique = function (nums, curr = [], res = [], indexArr = []) {
+  if (curr.length === nums.length) {
+    if (!res.some((a) => curr.every((v, i) => v === a[i]))) {
+      res.push(curr);
+    }
+    return;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (indexArr.includes(i)) continue;
+    permuteUnique(nums, [...curr, nums[i]], res, [...indexArr, i]);
+  }
+  return res;
+};
