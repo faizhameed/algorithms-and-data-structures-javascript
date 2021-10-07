3c644bfc6707b5c79d29d13272a9173ab95877c5
Faiz Hameed <faizftb@gmail.com>
2021-10-07 12:19:44 +0530

    space fix

--- leetcode/triangle.js
+++ leetcode/triangle.js
@@ -2,9 +2,6 @@
 
 For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
 
-
-
-
 */
 
 var minimumTotal = function (triangle) {
