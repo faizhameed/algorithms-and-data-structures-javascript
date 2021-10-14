e668f313aaa40851608a9b6b73e2bcfb2cf0c7ae
Faiz Hameed <faizftb@gmail.com>
2021-10-14 20:51:26 +0530

    optimized mergeKLists

--- leetcode/mergeKLists.js
+++ leetcode/mergeKLists.js
@@ -28,18 +28,24 @@ function merge(root, linkedList) {
     }
     cur3 = cur3.next;
   }
-  if (cur1) {
-    while (cur1) {
-      cur3.next = new ListNode(cur1.val);
-      cur1 = cur1.next;
-      cur3 = cur3.next;
-    }
-  } else if (cur2) {
-    while (cur2) {
-      cur3.next = new ListNode(cur2.val);
-      cur2 = cur2.next;
-      cur3 = cur3.next;
-    }
+  if (!cur1) {
+    cur3.next = cur2;
+  } else if (!cur2) {
+    cur3.next = cur1;
   }
   return newList.next;
+}
+
+/* Optimized solution */
+
+function mergekLists(lists) {
+  const amount = lists.length;
+  let interval = 1;
+  while (interval < amount) {
+    for (let i = 0; i < amount - interval; i += interval * 2) {
+      lists[i] = merge(lists[i], lists[i + interval]);
+    }
+    interval *= 2;
+  }
+  return amount > 0 ? lists[0] : null;
 }
