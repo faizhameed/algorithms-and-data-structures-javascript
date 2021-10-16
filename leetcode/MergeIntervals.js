66f1b7835ba0fa0bf1390325afd0feb7b4e81374
Faiz Hameed <faizftb@gmail.com>
2021-10-16 14:27:15 +0530

    fixing error

--- leetcode/MergeIntervals.js
+++ leetcode/MergeIntervals.js
@@ -1,20 +1,18 @@
 /* Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input. */
 
 var mergeIntervals = function (intervals) {
-  //   intervals.sort((a, b) => {
-  //     if (a[0] < b[0]) {
-  //       return -1;
-  //     }
-  //     return 1;
-  //   });
+  intervals.sort((a, b) => {
+    if (a[0] < b[0]) {
+      return -1;
+    }
+    return 1;
+  });
   const res = [intervals[0]];
 
   for (let i = 1; i < intervals.length; i++) {
     const size = res.length - 1;
     if (res[size][1] >= intervals[i][0]) {
-      // can we break this rule
       res[size][1] = Math.max(intervals[i][1], res[size][1]);
-      res[size][0] = Math.min(intervals[i][0], res[size][0]);
     } else {
       res.push(intervals[i]);
     }
