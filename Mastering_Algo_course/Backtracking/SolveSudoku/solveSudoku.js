bd128783b7ed7c167f08c850a7cb3b0f0917ebda
Faiz Hameed <faizftb@gmail.com>
2021-10-25 19:21:37 +0530

    solve sudoku

--- Mastering_Algo_course/Backtracking/SolveSudoku/solveSudoku.js
+++ Mastering_Algo_course/Backtracking/SolveSudoku/solveSudoku.js
@@ -1,7 +1,7 @@
 const getBoxId = (row, col) => {
-  const colVal = Math.floor(col / 3);
   const rowVal = Math.floor(row / 3) * 3;
-  return colVal + rowVal;
+  const colVal = Math.floor(col / 3);
+  return rowVal + colVal;
 };
 
 const isValid = (box, row, col, num) => {
@@ -96,5 +96,82 @@ const board = [
   [".", ".", ".", ".", "8", ".", ".", "7", "9"],
 ];
 
-solveSudoku(board);
+solveSudoku2(board);
 console.log(board);
+
+function sudokuBacktrack(board, boxes, rows, cols, r, c) {
+  if (r === board.length || c === board[0].length) {
+    return true;
+  }
+  if (board[r][c] === ".") {
+    const boxId = getBoxId(r, c);
+    const box = boxes[boxId];
+    const row = rows[r];
+    const col = cols[c];
+    for (let num = 1; num <= 9; num++) {
+      const numVal = num.toString();
+      board[r][c] = numVal;
+
+      if (isValid(box, row, col, numVal)) {
+        box[numVal] = true;
+        row[numVal] = true;
+        col[numVal] = true;
+
+        if (c === board[0].length - 1) {
+          if (sudokuBacktrack(board, boxes, rows, cols, r + 1, 0)) {
+            return true;
+          }
+        } else {
+          if (sudokuBacktrack(board, boxes, rows, cols, r, c + 1)) {
+            return true;
+          }
+        }
+
+        delete box[numVal];
+        delete row[numVal];
+        delete col[numVal];
+      }
+
+      board[r][c] = ".";
+    }
+  } else {
+    if (c === board[0].length - 1) {
+      if (sudokuBacktrack(board, boxes, rows, cols, r + 1, 0)) {
+        return true;
+      }
+    } else {
+      if (sudokuBacktrack(board, boxes, rows, cols, r, c + 1)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function solveSudoku2(board) {
+  const n = board.length;
+  const rows = new Array(n);
+  const cols = new Array(n);
+  const boxes = new Array(n);
+
+  for (let i = 0; i < n; i++) {
+    boxes[i] = {};
+    rows[i] = {};
+    cols[i] = {};
+  }
+
+  //initalise the current values
+  for (let r = 0; r < n; r++) {
+    for (let c = 0; c < n; c++) {
+      if (board[r][c] !== ".") {
+        const boxId = getBoxId(r, c);
+        const val = board[r][c];
+        boxes[boxId][val] = true;
+        rows[r][val] = true;
+        cols[c][val] = true;
+      }
+    }
+  }
+  sudokuBacktrack(board, boxes, rows, cols, 0, 0);
+}
