0abad74c04c30cc75c52c919f5fd214f6add2615
Faiz Hameed <faizftb@gmail.com>
2021-11-02 22:54:28 +0530

    build binary tree from given inorder and postorder arrays

--- leetcode/buildTree.js
+++ leetcode/buildTree.js
@@ -18,3 +18,24 @@ var buildTree = function (preorder, inorder) {
 /* 
 preorder: [1, 2, 4, 5, 3, 6];
 inorder: [4, 2, 5, 1, 6, 3]; */
+
+var buildTree = function (postorder, inorder) {
+  // root value = preOrder[0]
+  const map_inorder = {};
+
+  inorder.forEach((v, i) => {
+    map_inorder[v] = i;
+  });
+  function build(left, right) {
+    if (left > right) {
+      return null;
+    }
+    const root = new TreeNode(postorder.pop());
+
+    mid = map_inorder[root.val];
+    root.right = build(mid + 1, right);
+    root.left = build(left, mid - 1);
+    return root;
+  }
+  return build(0, inorder.length - 1);
+};
