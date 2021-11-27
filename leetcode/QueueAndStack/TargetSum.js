b7c991578a9ec3f8637a951a45526323154441af
Faiz Hameed <faizftb@gmail.com>
2021-11-27 20:55:39 +0530

    findtarget sum ways DP optimized

--- leetcode/QueueAndStack/TargetSum.js
+++ leetcode/QueueAndStack/TargetSum.js
@@ -1,6 +1,6 @@
 /* Brute force */
 
-var findTargetSumWays = function (nums, target) {
+var findTargetSumWaysBrute = function (nums, target) {
   let count = 0;
   function calculate(i, sum) {
     if (i === nums.length) {
@@ -18,4 +18,22 @@ var findTargetSumWays = function (nums, target) {
 
 const nums = [1, 1, 1, 1, 1],
   target = 3;
+
+var findTargetSumWays = function (nums, target) {
+  const map = new Map();
+  function calculate(i, sum) {
+    if (i === nums.length) {
+      return target === sum ? 1 : 0;
+    }
+    if (map.has(`${i}-${sum}`)) {
+      return map.get(`${i}-${sum}`);
+    }
+    const res =
+      calculate(i + 1, sum + nums[i]) + calculate(i + 1, sum - nums[i]);
+    map.set(`${i}-${sum}`, res);
+    return res;
+  }
+  return calculate(0, 0);
+};
+
 console.log(findTargetSumWays(nums, target));
