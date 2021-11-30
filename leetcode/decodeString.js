e11b4d534a8fee9fc62e366dbb1d525d58710815
Faiz <faizftb@gmail.com>
2021-11-30 21:19:13 +0530

    decode string stack

--- leetcode/decodeString.js
+++ leetcode/decodeString.js
@@ -1,24 +1,25 @@
-var decodeString = function (s) {
+const decodeString = (s) => {
   const stack = [];
-  let isStore = false,
-    temp = [],
-    decoded = "";
-  for (let i = 0; i < s.length; i++) {
-    const curr = s[i];
-    if (curr === "]") {
-      let temp = "",
-        popped = "";
-      while (popped !== "]") {
-        popped = stack.pop();
-        temp += popped;
-      }
-      const mult = stack.pop();
-      for (let i = 1; i <= mult; i++) {
-        decoded += temp;
-      }
-      decoded = decoded.split("").reverse().join("");
+  for (const char of s) {
+    if (char !== "]") {
+      stack.push(char);
+      continue;
     }
-  }
-};
+    let curr = stack.pop();
+    let str = "";
+    while (curr !== "[") {
+      str = curr + str;
+      curr = stack.pop();
+    }
+    let num = "";
+    curr = stack.pop();
 
-console.log(decodeString("3[a]2[bc]"));
+    while (!Number.isNaN(Number(curr))) {
+      num = curr + num;
+      curr = stack.pop();
+    }
+    stack.push(curr);
+    stack.push(str.repeat(Number(num)));
+  }
+  return stack.join("");
+};
