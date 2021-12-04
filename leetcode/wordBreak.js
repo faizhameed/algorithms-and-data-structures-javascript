b5eec428e3bb06aa72802c18fa55df85c50941d5
Faiz <faizftb@gmail.com>
2021-12-04 22:10:22 +0530

    word break

--- leetcode/wordBreak.js
+++ leetcode/wordBreak.js
@@ -32,21 +32,30 @@ const s = "applepenapple",
 // output true
 
 //  optimized solution
+const wordBreak = (s, wordDict) => {
+  if (wordDict == null || wordDict.length === 0) return false;
+  const set = new Set(wordDict);
 
-function wordBreak(s, wordDict, curr = "") {
-  if (s === curr) {
-    return true;
-  }
-  const len = curr.length;
-  if (s.length <= len) {
-    return false;
-  }
-  for (let i = 0; i < wordDict.length; i++) {
-    if (wordBreak(s, wordDict, curr.concat(wordDict[i]))) {
-      return true;
+  // When s = 'catsandog', wordDict = ['cats', 'ca', 'ts']
+  // After 'cats' and 'ca', it will become 'andog', 'tsandog'
+  // For 'tsandog', after 'ts', it will become 'andog' again, visited set here is for memoization
+  const visited = new Set(); // to avoid checking the same rest of the string if its not there
+  const q = [0];
+
+  while (q.length) {
+    const start = q.shift();
+
+    if (!visited.has(start)) {
+      for (let end = start + 1; end <= s.length; end++) {
+        if (set.has(s.slice(start, end))) {
+          if (end === s.length) return true;
+          q.push(end);
+        }
+      }
+      visited.add(start);
     }
   }
   return false;
-}
+};
 
 console.log(wordBreak(s, wordDict));
