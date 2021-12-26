75fdfdf64acf6b22c9986eeccd94d48c8173021f
Faiz <faizftb@gmail.com>
2021-12-26 22:00:39 +0530

    coin change

---
+++ leetcode/Hard/coinChange.js
@@ -0,0 +1,19 @@
+/* 322. Coin Change */
+
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (i - coins[j] >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+      }
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
+
+const coins = [1, 2, 5],
+  amount = 11;
+
+console.log(coinChange(coins, amount));
