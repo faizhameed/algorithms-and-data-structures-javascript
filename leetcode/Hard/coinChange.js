d0eb7fb167789e71399c3649a043e154c7df60c6
Faiz <faizftb@gmail.com>
2021-12-27 21:17:23 +0530

    coin change brute solution

--- leetcode/Hard/coinChange.js
+++ leetcode/Hard/coinChange.js
@@ -1,5 +1,26 @@
 /* 322. Coin Change */
 
+/* Brute force */
+
+function coinChangeBrute(coins, amount) {
+  if (amount === 0) return 0;
+  let min = Infinity;
+  coinRecurse(amount, 0);
+
+  function coinRecurse(currAmt, count) {
+    if (currAmt === 0) return 0;
+    if (currAmt < 0) return -1;
+
+    for (let i = 0; i < coins.length; i++) {
+      const coin = coins[i];
+      const res = coinRecurse(currAmt - coin, count + 1);
+      if (res < 0) continue;
+      if (res === 0) min = Math.min(min, count + 1);
+    }
+  }
+  return min === Infinity ? -1 : min;
+}
+
 function coinChange(coins, amount) {
   const dp = new Array(amount + 1).fill(Infinity);
   dp[0] = 0;
