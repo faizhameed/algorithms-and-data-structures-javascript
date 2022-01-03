dcd544eb55d9df705cb6c1ea09a6e1ae1eb5e0c8
Faiz Hameed <faizftb@gmail.com>
2022-01-03 10:52:02 +0530

    serialize and deserialize optimal

--- leetcode/OOP/SerializeAndDeserialize.js
+++ leetcode/OOP/SerializeAndDeserialize.js
@@ -1,7 +1,28 @@
+/* BST can be stringified after preOrder array and then converted back by postOrder
+operation */
+
 var serialize = function (root) {
-  return JSON.stringify(root);
+  const preOrder = [];
+  function dfs(node) {
+    if (node === null) return null;
+    preOrder.push(node.val);
+    dfs(node.left);
+    dfs(node.right);
+  }
+  dfs(root);
+  return preOrder.join(",");
 };
 
 var deserialize = function (data) {
-  return JSON.parse(data);
+  if (data === "") return null;
+  const preOrder = data.split(",");
+  function recur(lower, upper) {
+    if (Number(preOrder[0]) < lower || Number(preOrder[0]) > upper) return null;
+    if (preOrder.length === 0) return null;
+    const root = new TreeNode(preOrder.shift());
+    root.left = dfs(root.left, upper);
+    root.right = dfs(lower, root.right);
+    return root;
+  }
+  return recur(-Infinity, Infinity);
 };
