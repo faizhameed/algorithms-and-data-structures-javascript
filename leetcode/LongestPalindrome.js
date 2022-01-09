69c8bdcc0663d877320dfc8ab0519ad152660ffa
Faiz <faizftb@gmail.com>
2022-01-09 21:16:41 +0530

    Robot bound

--- leetcode/LongestPalindrome.js
+++ leetcode/LongestPalindrome.js
@@ -1,5 +1,5 @@
 var longestPalindrome = function (words) {
-  // store the word in a map with its pair
+  // store the word in a map with total count
   const map = {};
   let count = 0;
 
@@ -9,18 +9,18 @@ var longestPalindrome = function (words) {
       map[word] += 1;
     }
   }
-  /* 
-  if words.length is odd then we need a word with same letters 
-  */
+
   let flag = false;
   for (const word of words) {
-    const reverse = word.split("").reverse().join("");
+    const reverse = word[1] + word[0];
     if (word === reverse) {
       while (map[word] >= 2) {
         map[word] -= 2;
         count += 4;
       }
       if (map[word] === 1 && !flag) {
+        // only we need to take one word with same pairs which give us an advantage for center letter pair
+        // anymore similar pair will not help so we make flag to true
         flag = true;
         count += 2;
         map[word] -= 1;
