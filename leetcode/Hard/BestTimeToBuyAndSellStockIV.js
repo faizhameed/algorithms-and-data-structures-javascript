95af31f94fe7ce300d108826923864a0a6f8fa37
Faiz <faizftb@gmail.com>
2022-01-18 21:48:25 +0530

    Best time to buy and sell stock 4

--- leetcode/Hard/BestTimeToBuyAndSellStockIV.js
+++ leetcode/Hard/BestTimeToBuyAndSellStockIV.js
@@ -26,6 +26,33 @@ var maxProfit = function (k, prices) {
   }
 };
 
+/* Bottom up approach */
+
+var maxProfitBU = function (k, prices) {
+  const n = prices.length;
+  const dp = new Array(n + 1)
+    .fill(0)
+    .map(() => new Array(k + 1).fill(0).map(() => new Array(2).fill(0)));
+
+  for (let i = n - 1; i >= 0; i--) {
+    for (let tr = 1; tr <= k; tr++) {
+      for (let h = 0; h < 2; h++) {
+        const doNothing = dp[i + 1][tr][h];
+        let doSomething;
+        if (h === 0) {
+          //buying
+          doSomething = -prices[i] + dp[i + 1][tr][1];
+        } else {
+          doSomething = prices[i] + dp[i + 1][tr - 1][0];
+        }
+        dp[i][tr][h] = Math.max(doNothing, doSomething);
+      }
+    }
+  }
+
+  return dp[0][k][0];
+};
+
 const k = 2,
-  prices = [2, 4, 1];
-console.log(maxProfit(k, prices));
+  prices = [3, 3, 5, 0, 0, 3, 1, 4];
+console.log(maxProfitBU(k, prices));
