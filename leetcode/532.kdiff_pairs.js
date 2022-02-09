a7d93cc67ad40153fc7b4919229bbf8069d127d2
Faiz <faizftb@gmail.com>
2022-02-09 21:52:39 +0530

    532 k diff pairs optimized

--- leetcode/532.kdiff_pairs.js
+++ leetcode/532.kdiff_pairs.js
@@ -1,4 +1,4 @@
-var findPairs = function (nums, k) {
+var findPairsBrute = function (nums, k) {
   let count = 0;
   const seen = {};
   for (let i = 0; i < nums.length - 1; i++) {
@@ -12,3 +12,29 @@ var findPairs = function (nums, k) {
   }
   return count;
 };
+
+/* Optimized */
+
+var findPairs = function (nums, k) {
+  let count = 0;
+  const seen = {};
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!map[nums[i]]) {
+      map[nums[i]] = 1;
+    } else map[nums[i]] += 1;
+  }
+  for (let x of Object.keys(map)) {
+    x = Number(x);
+    if (k > 0 && map[x + k]) {
+      count++;
+    } else if (k === 0 && map[x] > 1) {
+      count++;
+    }
+  }
+  return count;
+};
+const nums = [1, 3, 1, 5, 4],
+  k = 0;
+console.log(findPairs(nums, k));
