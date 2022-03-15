36ac6ed61d9afb4f1f4efe7701c7f09ce0402ca3
Faiz <faizftb@gmail.com>
2022-03-15 13:28:17 +0530

    cheapest flights with bellman

--- leetcode/Graph/787.cheapestFlightsKstops.js
+++ leetcode/Graph/787.cheapestFlightsKstops.js
@@ -57,3 +57,35 @@ var findCheapestPrice = function (n, flights, src, dst, k) {
   }
   return -1;
 };
+
+/* Bellman's ford approach */
+
+var findCheapestPriceBellman = function (n, flights, src, dst, k) {
+  let pre = new Array(n).fill(Infinity);
+  let curr = new Array(n).fill(Infinity);
+
+  pre[src] = 0;
+  // we will have k+1 edges for k stops
+  for (let i = 1; i <= k + 1; i++) {
+    curr[src] = 0;
+    for (const [pre_f, curr_f, cost] of flights) {
+      if (pre[pre_f] < Infinity) {
+        curr[curr_f] = Math.min(pre[pre_f] + cost, curr[curr_f]);
+      }
+    }
+    // we clone to previouse
+    pre = [...curr];
+  }
+  return curr[dst] === Infinity ? -1 : curr[dst];
+};
+
+const n = 3,
+  flights = [
+    [0, 1, 100],
+    [1, 2, 100],
+    [0, 2, 500],
+  ],
+  src = 0,
+  dst = 2,
+  k = 1;
+console.log(findCheapestPriceBellman(n, flights, src, dst, k));
