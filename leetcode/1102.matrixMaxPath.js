35223983299470f5932cef12ac476da485fa76c6
Faiz <faizftb@gmail.com>
2022-03-30 22:23:01 +0530

    matrix min max path

--- leetcode/1102.matrixMaxPath.js
+++ leetcode/1102.matrixMaxPath.js
@@ -17,23 +17,17 @@ var maximumMinimumPath = function (grid) {
 
     while (queue.length) {
       const [curr_row, curr_col] = queue.shift();
-      console.log(curr_row, curr_col);
       if (curr_row === R - 1 && curr_col === C - 1) return true;
-      visited[curr_row][curr_col] = true;
       for (const [dx, dy] of dirs) {
         const new_row = dx + curr_row;
         const new_col = dy + curr_col;
-        if (
-          new_row < 0 ||
-          new_row >= R ||
-          new_col < 0 ||
-          new_col >= C ||
-          score > grid[new_row][new_col] ||
-          visited[new_row][new_col]
-        ) {
+        if (new_row < 0 || new_row >= R || new_col < 0 || new_col >= C) {
           continue;
         }
-        queue.push([new_row, new_col]);
+        if (grid[new_row][new_col] >= score && !visited[new_row][new_col]) {
+          queue.push([new_row, new_col]);
+          visited[new_row][new_col] = true;
+        }
       }
     }
     return false;
