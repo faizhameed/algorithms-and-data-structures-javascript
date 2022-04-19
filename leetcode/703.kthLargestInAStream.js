11a3d53ac01c7e41506448ccf6aeebb416ce0ae7
Faiz <faizftb@gmail.com>
2022-04-19 09:42:06 +0530

    Kth largest element in a stream

--- leetcode/703.kthLargestInAStream.js
+++ leetcode/703.kthLargestInAStream.js
@@ -3,12 +3,26 @@ var KthLargest = function (k, nums) {
   this.k = k;
 };
 
-/**
- * @param {number} val
- * @return {number}
- */
 KthLargest.prototype.add = function (val) {
   this.items.push(val);
   this.items.sort((a, b) => b - a);
   return this.items[this.k - 1];
+};
+
+// need to write optimized solution for this.
+
+var KthLargest = function (k, nums) {
+  this.k = k;
+  this.heap = new MinHeap();
+  nums.forEach((n) => this.add(n));
+};
+
+KthLargest.prototype.add = function (val) {
+  if (this.heap.size() < this.k) {
+    this.heap.offer(val);
+  } else if (this.heap.peek() < val) {
+    this.heap.offer(val);
+    this.heap.poll();
+  }
+  return this.heap.peek();
 };
