5e62ee482707e185167457d647dfe191819e3929
Faiz <faizhameedv@gmail.com>
2022-09-15 23:00:12 +0400

    967 to be cntd

--- leetcode/InCompleteProblems/967NumsWithSameConsDiff.js
+++ leetcode/InCompleteProblems/967NumsWithSameConsDiff.js
@@ -1,5 +1,29 @@
 var numsSameConsecDiff = function (n, k) {
   //numbers with consecutive differences
-
-  const ans = [];
+  const res = [];
+  for (let i = 1; i <= 9; i++) {
+    let first = i,
+      second = i + k === 10 ? 0 : i + k;
+    for (let k = 0; k < 2; k++) {
+      if (second < 10 && second !== 0) {
+        // console.log(first, second);
+        let num = 0,
+          flag = 1;
+        for (let j = 0; j < n; j++) {
+          if (flag) {
+            num += first * Math.pow(10, j);
+            flag = 0;
+          } else {
+            num += second * Math.pow(10, j);
+            flag = 1;
+          }
+        }
+        res.push(num);
+      }
+      [first, second] = [second, first];
+    }
+  }
+  return res;
 };
+
+console.log(numsSameConsecDiff(3, 1));
